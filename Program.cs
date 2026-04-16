d2412c0b11f8e5fe197dfc9d8979a41f6464a6ac
LeoGPrado <leogprado28@gmail.com>
2026-04-16 17:39:56 -0500

    Base

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace Pruebas
     {
         static void Main(string[] args)
         {
+            
+             Valley valley = new Valley();
+             valley.Play();
+            
         }
     }
 }
---
+++ Valley.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    internal class Valley : Scene
+    {
+        public void Play()
+        {
+            Console.WriteLine("Estas en un valle...");
+            Console.WriteLine("Decides caminar por el campo.");
+            Console.ReadLine();
+        }
+
+    }
+
+}
